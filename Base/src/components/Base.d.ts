8432269d0792d78ffedf81923c8ef131684a753c
Jerome Vercide <jerome8vercide@yahoo.com>
2024-07-21 20:03:16 -0400

    Updates

---
+++ Base/src/components/Base.d.ts
@@ -0,0 +1,6 @@
+import { FC } from '../../../../node_modules/react';
+
+export interface BaseProps {
+}
+declare const Base: FC<BaseProps>;
+export default Base;
---
+++ Base/src/components/index.stories.d.ts
@@ -0,0 +1,6 @@
+import { Meta } from '@storybook/react';
+import { Base } from '.';
+
+declare const _default: Meta<typeof Base>;
+export default _default;
+export declare const PackageVariant: import('@storybook/types').AnnotatedStoryFn<import('@storybook/react').ReactRenderer, import('./Base').BaseProps>;
